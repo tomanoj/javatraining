0bec296195af974a7e292e6a6547352eabc24a91
tomanoj <67562817+tomanoj@users.noreply.github.com>
2020-07-11 16:39:22 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Adding another code");
+		System.out.println("Adding code from Github.")
 	}
 
 }
